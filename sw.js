78881615e1366b111f6a5e6f9704c605b5c3dab1
almolikeym025-del <almolikeym025@gmail.com>
2026-01-09 18:36:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const cacheName='finance-app-v1';
+const files=['./','index.html','manifest.json'];
+
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open(cacheName).then(c=>c.addAll(files)));
+});
+
+self.addEventListener('fetch',e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
+});
